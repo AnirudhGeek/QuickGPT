caf759fffe1a72f6e90839096453fa10fa20ba52
Anirudh Raturi <annigeek@gmail.com>
2025-08-30 04:14:17 +0530

    frontend completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,26 @@
 import React, { useState } from "react";
 import Sidebar from "./components/Sidebar";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import ChatBox from "./components/ChatBox";
 import Credits from "./pages/Credits";
 import Community from "./pages/Community";
 import { assets } from "./assets/assets";
-import './assets/prism.css'
+import "./assets/prism.css";
+import Loading from "./pages/Loading";
+import { useAppContext } from "./context/AppContext";
+import Login from "./pages/Login";
 
 const App = () => {
+
+  const {user} = useAppContext()
+
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { pathname } = useLocation();
+
+  if (pathname === "/loading") {
+    return <Loading />;
+  }
+
   return (
     <>
       {/* Mobile Menu Button - Only show when sidebar is closed */}
@@ -20,17 +32,24 @@ const App = () => {
           alt="Menu"
         />
       )}
-      
+
+      {user?(
       <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
         <div className="flex h-screen w-screen">
           <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
           <Routes>
             <Route path="/" element={<ChatBox />} />
-            <Route path="/credits" element={<Credits />} />
+            <Route path="/credit" element={<Credits />} />
             <Route path="/community" element={<Community />} />
           </Routes>
         </div>
       </div>
+      ) :(
+        <div className="bg-gradient-to-b from-[#242124] to-[#000000] flex items-center justify-center h-screen w-screen">
+          <Login/>
+        </div>
+      )}
+
     </>
   );
 };
